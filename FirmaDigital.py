02bcbfe5b317b28269229a3c1b99efbe705dd141
Yose Sotomayor <120493435+Yosesotomayor@users.noreply.github.com>
2025-05-22 10:13:22 -0600

    caray

--- FirmaDigital.py
+++ FirmaDigital.py
@@ -236,9 +236,6 @@ if not st.session_state.logged_in:
     with tabs[0]:
         df_usuarios = load_users()
         df_usuarios["password_str"] = df_usuarios["password"].apply(lambda x: bytes(map(int, x.split(","))).decode())
-        df_usuarios["unhashed_password"] = df_usuarios["password"].apply(
-            lambda x: bcrypt.hashpw(x.encode(), bcrypt.gensalt()).decode()
-        )
         st.subheader("📋 Debug - Usuarios desde Azure Table")
         st.dataframe(df_usuarios)
         st.session_state.role = "IniciarSesion"
